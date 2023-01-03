e6a2e4ebea4673f2c6b4b75f439fe5eb4af47474
grant8827@yahoo.com <77510135+grant8827@users.noreply.github.com>
2023-01-02 19:25:27 -0500

    complete about page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let bars = document.getElementById("toggle-links");
+    if (bars.style.display === "block") {
+        bars.style.display = "none";
+    } else {
+        bars.style.display = "block";
+    }
+}
